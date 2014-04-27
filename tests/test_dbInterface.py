3d5d06c51e34a2e3112cf01652316eccbeb60358
Jesse S (Laptop) <jelloeater@gmail.com>
2014-04-27 10:17:27 +0400

    Tired Code :(

--- tests/test_dbInterface.py
+++ tests/test_dbInterface.py
@@ -1,6 +1,9 @@
 import os
 from unittest import TestCase
 
+from forgeLandWall import dbInterface
+
+
 __author__ = 'Jesse'
 
 
@@ -10,9 +13,38 @@ class TestdbInterface(TestCase):
 		os.chdir("..")  # Go-to project root
 		os.chdir("forgeLandWall")
 
+		import forgeLandWall.models as models
+
+		x=models.messageModel()
+		x.message("json1")
+		x=models.messageModel()
+		x.message("json2")
+		x=models.messageModel()
+		x.message("json3")
+		print("TEST SETUP")
+
 	def tearDown(self):
-		pass
+		"""Delete test messages"""
+		import forgeLandWall.dbConnManage as dbConnManage
+		dbConn, dbcursor = dbConnManage.dbConnect()
+		messageStr = "json"
+		sqlStr = 'DELETE FROM messages WHERE "message" LIKE "' + messageStr + '";'
+		dbcursor.execute(sqlStr)
+		dbConnManage.dbClose(dbConn)
+		print("TEST TEARDOWN")
 
 	def test_getMessagesFromDBasJSONObjectArray(self):
-		self.fail(msg="Unfinished Test")
+		import json
+
+		retPut = dbInterface.getMessagesFromDBasJSONObjectArray(3)
+		# Get the Data from the db
+
+		ObjList = json.loads(retPut)
+		testStr = ObjList[0].message
+		print("JSON: " + retPut)
+		print("JSONtoObj: " + testStr)
+		if retPut == testStr:
+			pass
+		else:
+			self.fail(msg="Unfinished Test")
 		# TODO Write JSON unit test
