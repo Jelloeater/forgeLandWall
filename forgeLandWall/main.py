9541806db41ff3bd1c0b04bb9d9860b748b0a1b6
Jesse S <jelloeater@gmail.com>
2014-04-29 07:01:49 +0400

    Cleaned up main module

--- forgeLandWall/main.py
+++ forgeLandWall/main.py
@@ -1,7 +1,5 @@
 from wsgiref.simple_server import make_server
-
 import forgeLandWall.settings as settings
-
 
 __author__ = 'Jesse'
 
@@ -9,38 +7,14 @@ import forgeLandWall.dbSetup as dbSetup
 import forgeLandWall.views as views
 
 
-
-
 def main():
 	ip = settings.getIpAddress()
 	port = settings.globalVars.portNumber
 	dbSetup.setupDB()
 
-	# TEST CODE
-	# TODO Need to test this code
-
-	# dbObj = models.messageModel()
-	# dbObj.message("dogs")
-
-	# dbObj2 = models.messageModel(21)
-	# dbObj2.message("newMessage")
-	#
-	# dbObj3 = models.messageModel(20)
-	# dbObj3.deleteRecord()
-	#
-	# dbObj4 = models.messageModel(message="fuck")
-	# print(dbObj4.message() + " " + dbObj4.getTimestamp())
-	#
-	# dbObj4 = models.messageModel(message="new")
-	# print(dbObj4.message() + " " + dbObj4.getTimestamp())
-	#
-	# msg = models.messageModel(30)
-	# print(msg.message() + msg.getTimestamp())
-
-
 	print("Serving on: http://" + str(ip) + ":" + str(port))
 	httpd = make_server(ip, port, webHandler)
-	# httpd.serve_forever()
+	httpd.serve_forever()
 
 
 class webHandler:
