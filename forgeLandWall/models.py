97b75a4dce78652edfc182f7a8986bb8b6853796
Jesse S (Laptop) <jelloeater@gmail.com>
2014-05-01 04:01:53 +0400

    Removed lookup record via text (it can return the wrong message if it gets an early match)

--- forgeLandWall/models.py
+++ forgeLandWall/models.py
@@ -10,7 +10,7 @@ __author__ = 'Jesse'
 
 
 class messageModel(globalVars):
-	# Creates record handle OR reads record from db
+	# Creates record handle OR reads record from db, we shouldn't work with more then one record
 	"""Represents a SINGLE record from the table, we manipulate the objects, rather then SQL"""
 
 	def __init__(self, index=None, message=None):
@@ -22,8 +22,6 @@ class messageModel(globalVars):
 		if index is None and message is None:
 			self.__index = None
 
-		if message is not None and index is None:
-			self.__lookupRecordFromMessage(message)
 		if index is not None and message is None:
 			# Index should still be used for edits and delete though
 			self.__index = index
@@ -40,28 +38,6 @@ class messageModel(globalVars):
 
 	def getTimestamp(self):
 		return self.__timestamp
-
-
-	def __lookupRecordFromMessage(self, searchStr):
-		"""Looks up ONLY the FIRST record that matches the search"""
-
-		dbConn, dbcursor = dbConnection.dbConnect()
-		if globalVars.debugMode: print("Searching for: " + searchStr)
-		try:
-			sqlStr = 'SELECT * FROM messages WHERE message LIKE"%' + searchStr + '%"'
-			dbcursor.execute(sqlStr)
-			record = dbcursor.fetchone()
-			self.__messageTxt = record[0]
-			self.__timestamp = record[1]
-			self.__index = record[2]
-			if globalVars.debugMode: print("Record Found: " + searchStr)
-		except TypeError:
-			# FIXME Add better exception, no Pokemon exceptions!
-			self.__messageTxt = "CANNOT FIND MESSAGE: " + searchStr
-			self.__timestamp = ""
-			self.__index = ""
-			if globalVars.debugMode: print("Record Not Found")
-		dbConnection.dbClose(dbConn)
 
 
 	def __lookupRecordFromIndex(self):  # READ
