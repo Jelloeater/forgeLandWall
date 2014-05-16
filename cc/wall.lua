aa0dcd59bcf8d77af337ed3740f0e28e070d3634
Jesse S (Laptop) <jelloeater@gmail.com>
2014-05-17 02:15:53 +0400

    Started on client

--- cc/wall.lua
+++ cc/wall.lua
@@ -1,19 +1,3 @@
-print ("Enter IP address")
-ip = read()
-print ("Enter Port")
-port = read()
-
-url = "http://"..ip..":"..port
-print("Connecting to: " .. url)
-
-while true do
-	print ("Enter POST data")
-    --noinspection GlobalCreationOutsideO
-    postData = read()
-	http.post(url, postData)
-end
-
-
 -- ###GET
 -- "/get/numberOfPostsToGetAsJSON"
 -- "/query/messageToSearchFor"
@@ -26,29 +10,88 @@ end
 
 os.loadAPI('jsonV2')
 
+-----------------------------------------------------------------------------------------------------------------------
+-- Settings Class
+settings = {}  -- the table representing the class, holds all the data, we don't need a singleton because THIS IS LUA.
+settings.serverURL = 'http://127.0.0.1:9000'
 
-x = http.get('http://127.0.0.1:9000/get/2')
-jsonStr = x.readAll()
--- print(jsonStr)
-jsonObj = jsonV2.decode(jsonStr)
-for i=1,table.getn(jsonObj) do
-    print(jsonObj[i].index .. " "..jsonObj[i].message .. " ".. jsonObj[i].timestamp)
-end
--- print(jsonObj.readAll())
+-----------------------------------------------------------------------------------------------------------------------
+-- Message Class
+local Message = {}  -- the table representing the class, which will double as the metatable for the instances
+Message.__index = Message -- failed table lookups on the instances should fallback to the class table, to get methods
 
-x = http.get('http://127.0.0.1:9000/msg/2')
-jsonStr = x.readAll()
--- print(jsonStr)
-jsonObj = jsonV2.decode(jsonStr)
-print (jsonObj.message)
-
-x = http.get('http://127.0.0.1:9000/query/me')
-jsonStr = x.readAll()
--- print(jsonStr)
-jsonObj = jsonV2.decode(jsonStr)
-for i=1,table.getn(jsonObj) do
-    print(jsonObj[i].index .. " "..jsonObj[i].message .. " ".. jsonObj[i].timestamp)
+function Message.new(message, timestamp, index)
+	local self = setmetatable({},Message) 
+	-- Lets class self refrence to create new objects based on the class	
+	self.message = message or nil
+	self.timestamp = timestamp or nil
+	self.index = index or nil
+	return self
 end
 
-msgIn = read()
-http.post('http://127.0.0.1:9000/post','create='..msgIn)
+
+function createMessage()
+	print("Enter index to be removed: ")
+	local message = read()
+	http.post(settings.serverURL..'/post','create='..message)
+end
+
+function removeMessage()
+	print("Enter index to be removed: ")
+	local removeIndex = read()
+	http.post(settings.serverURL..'/post','delete='..removeIndex)
+end
+
+function editMessage( )
+	-- edit=newMessage&index=indexToEdi
+
+end
+
+function getMessages(numberToGet)
+	-- Gets message table
+	x = http.get(settings.serverURL..'/get/'.. numberToGet)
+	jsonStr = x.readAll()
+	jsonObj = jsonV2.decode(jsonStr)
+
+	messageTable = {} -- Clears the table
+	for i=1,table.getn(jsonObj) do
+		table.insert(messageTable, Message.new(
+			jsonObj[i].message, jsonObj[i].timestamp,jsonObj[i].index))
+	end
+end
+
+
+
+
+function main( ... )
+	getMessages()
+	
+
+
+	-- print(jsonObj.readAll())
+
+	x = http.get('http://127.0.0.1:9000/msg/2')
+	jsonStr = x.readAll()
+	-- print(jsonStr)
+	jsonObj = jsonV2.decode(jsonStr)
+	print (jsonObj.message)
+
+	x = http.get('http://127.0.0.1:9000/query/me')
+	jsonStr = x.readAll()
+	-- print(jsonStr)
+	jsonObj = jsonV2.decode(jsonStr)
+	for i=1,table.getn(jsonObj) do
+		print(jsonObj[i].index .. " "..jsonObj[i].message .. " ".. jsonObj[i].timestamp)
+	end
+
+	msgIn = read()
+	http.post('http://127.0.0.1:9000/post','create='..msgIn)
+
+end
+
+
+
+
+main()
+
+
